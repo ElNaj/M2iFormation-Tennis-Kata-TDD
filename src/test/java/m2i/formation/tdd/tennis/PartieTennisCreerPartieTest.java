fdeb480d9398ca20534a1fc213dcfc32c2c742c0
ElNaj <nelfateoui@gmail.com>
2022-07-19 15:41:16 +0200

    feat:najim: Ajout des classes de tests pour partie de tennis

---
+++ src/test/java/m2i/formation/tdd/tennis/PartieTennisCreerPartieTest.java
@@ -0,0 +1,11 @@
+package m2i.formation.tdd.tennis;
+
+import org.junit.jupiter.api.DisplayName;
+
+@DisplayName("Créer une nouvelle partie de tennis")
+public class PartieTennisCreerPartieTest {
+    
+    private static Joueur joueur1 = new Joueur();
+    private static Joueur joueur2 = new Joueur();
+    private static CompteurDeScoreTennis compteurScore = new CompteurDeScoreTennis();
+}
