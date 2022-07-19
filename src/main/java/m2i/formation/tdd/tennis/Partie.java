4fa7924f44c929a059e920456d2b683fee2d9c8e
loic <loic.crbn@outlook.com>
2022-07-19 14:42:51 +0200

    Add getters and setters to entities

--- src/main/java/m2i/formation/tdd/tennis/Partie.java
+++ src/main/java/m2i/formation/tdd/tennis/Partie.java
@@ -4,20 +4,81 @@ package m2i.formation.tdd.tennis;
  * Classe Partie, partie de tennis entre 2 joueurs avec scores
  */
 public class Partie {
-
-    //Propriétés 
     private Joueur joueur1;
     private Joueur joueur2;
     private Joueur gagnant;
 
-    private Score scoreJoueur1 = new Score();
-	private Score scoreJoueur2 = new Score();
-	
-	private boolean jeuxDecisif;
+    private Score scoreJoueur1 = new Score(0,0,0,false);
+    private Score scoreJoueur2 = new Score(0,0,0,false);
 
-    //Constructeur à 2 arguments (joueur1, joueur2)
-    // Todo
+    private boolean jeuxDecisif;
 
-    //Getters & Setters
-    //Todo
+    public Partie(Joueur joueur1, Joueur joueur2) {
+        this.joueur1 = joueur1;
+        this.joueur2 = joueur2;
+        this.jeuxDecisif = false;
+    }
+
+    public Joueur getJoueur1() {
+        return joueur1;
+    }
+
+    public void setJoueur1(Joueur joueur1) {
+        this.joueur1 = joueur1;
+    }
+
+    public Joueur getJoueur2() {
+        return joueur2;
+    }
+
+    public void setJoueur2(Joueur joueur2) {
+        this.joueur2 = joueur2;
+    }
+
+    public Joueur getGagnant() throws NullPointerException {
+        if(gagnant == null){
+            throw new NullPointerException("Aucun gagnant, la partie n'est pas finie");
+        }
+        return gagnant;
+    }
+
+    public void setGagnant(Joueur gagnant) {
+        this.gagnant = gagnant;
+    }
+
+    public Score getScoreJoueur1() {
+        return scoreJoueur1;
+    }
+
+    public void setScoreJoueur1(Score scoreJoueur1) {
+        this.scoreJoueur1 = scoreJoueur1;
+    }
+
+    public Score getScoreJoueur2() {
+        return scoreJoueur2;
+    }
+
+    public void setScoreJoueur2(Score scoreJoueur2) {
+        this.scoreJoueur2 = scoreJoueur2;
+    }
+
+    public boolean isJeuxDecisif() {
+        return jeuxDecisif;
+    }
+
+    public void setJeuxDecisif(boolean jeuxDecisif) {
+        this.jeuxDecisif = jeuxDecisif;
+    }
+
+    @Override
+    public String toString() {
+        return "Partie{" +
+                "joueur1=" + joueur1 +
+                ", joueur2=" + joueur2 +
+                ", gagnant=" + gagnant +
+                ", scoreJoueur1=" + scoreJoueur1 +
+                ", scoreJoueur2=" + scoreJoueur2 +
+                ", jeuxDecisif=" + jeuxDecisif +
+                '}';
+    }
 }
--- src/main/java/m2i/formation/tdd/tennis/Score.java
+++ src/main/java/m2i/formation/tdd/tennis/Score.java
@@ -4,12 +4,60 @@ package m2i.formation.tdd.tennis;
  * Classe Score du joueur
  */
 public class Score {
-    
     private int point;
     private int jeux;
     private int set;
     private boolean avantage;
 
-    //Getters & Setters
-    //Todo
+    public Score() {
+    }
+
+    public Score(int point, int jeux, int set, boolean avantage) {
+        this.point = point;
+        this.jeux = jeux;
+        this.set = set;
+        this.avantage = avantage;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+
+    public int getJeux() {
+        return jeux;
+    }
+
+    public void setJeux(int jeux) {
+        this.jeux = jeux;
+    }
+
+    public int getSet() {
+        return set;
+    }
+
+    public void setSet(int set) {
+        this.set = set;
+    }
+
+    public boolean isAvantage() {
+        return avantage;
+    }
+
+    public void setAvantage(boolean avantage) {
+        this.avantage = avantage;
+    }
+
+    @Override
+    public String toString() {
+        return "Score{" +
+                "point=" + point +
+                ", jeux=" + jeux +
+                ", set=" + set +
+                ", avantage=" + avantage +
+                '}';
+    }
 }
